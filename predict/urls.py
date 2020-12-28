551abe5d44ac89c59af06b0864f8d3ce87ea7d33
Metin Lamby <metin.lamby@outlook.com>
2020-12-28 15:44:57 +0100

    added the alibi model

--- predict/urls.py
+++ predict/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'predict'
 
 urlpatterns = [
-    path('', views.predict, name='predict')
+    path('', views.predict, name='predict'),
+    path('predict/', views.predict_chances, name='submit_prediction'),
 ]
+
+##path('', views.predict, name='prediction_page'),
+##path('results/', views.view_results, name='results'),
