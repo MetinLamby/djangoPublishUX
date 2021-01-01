43716c92bf166ec396846b66ab8b7b398135e494
Metin Lamby <metin.lamby@outlook.com>
2021-01-01 22:05:30 +0100

    added the new model

--- predict/views.py
+++ predict/views.py
@@ -19,7 +19,7 @@ def predict_chances(request):
         petal_width = float(request.POST.get('petal_width'))
 
         # Unpickle model
-        model = pd.read_pickle('predict/new_model.pickle')
+        model = pd.read_pickle('predict/new_model_two.pickle')
         # Make prediction
         result = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])
 
