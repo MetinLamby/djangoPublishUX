bd7f85aa4a15bfd33a908ea529390e9c5b7981a6
Metin Lamby <metin.lamby@outlook.com>
2021-01-01 20:23:01 +0100

    changed settings

--- djangoFrontend/settings.py
+++ djangoFrontend/settings.py
@@ -122,5 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ["/Users/MetinLamby/code/Metinlamby/pythonCode/newVirtualEnv/src/predict/static"]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+##STATICFILES_DIRS = ["/Users/MetinLamby/code/Metinlamby/pythonCode/newVirtualEnv/src/predict/static"]
